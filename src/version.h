17f28dd01aee591e93f4cf09f943e019d5448430
CryptoXploit <80972170+CryptoXploit@users.noreply.github.com>
2021-03-21 01:37:11 +0530

    Add files via upload

---
+++ src/version.h
@@ -0,0 +1,35 @@
+/*
+Bruteforce a wallet file.
+
+Copyright 2014-2020 Guillaume LE VAILLANT
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Additional permission under GNU GPL version 3 section 7
+
+If you modify this program, or any covered work, by linking or combining
+it with the OpenSSL library (or a modified version of that library),
+containing parts covered by the terms of the OpenSSL license, the licensors
+of this program grant you additional permission to convey the resulting work.
+Corresponding source for a non-source form of such a combination shall include
+the source code for the parts of the OpenSSL library used as well as that of
+the covered work.
+*/
+
+#ifndef VERSION_H
+#define VERSION_H 1
+
+#define VERSION_NUMBER "1.5.3"
+
+#endif
